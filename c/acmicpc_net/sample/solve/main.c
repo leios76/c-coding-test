8578e9fd82cfe0cdf7a034ecac111e5ad095399e
Minsoo Kim <ms.kim@hdel.co.kr>
2024-01-25 08:27:00 +0900

    sample 기본 템플릿 수정

--- c/acmicpc_net/sample/solve/main.c
+++ c/acmicpc_net/sample/solve/main.c
@@ -3,11 +3,27 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+struct context_t {
+    int n;
+};
+struct context_t context;
+
+int solve(struct context_t * ctx)
+{
+    return 0;
+}
+
+void init_data(struct context_t * ctx)
+{
+    memset(ctx, 0, sizeof(struct context_t));
+
+    scanf("%d", &ctx->n);
+}
+
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    printf("\n");
+    init_data(&context);
+    printf("%d\n", solve(&context));
 
     return 0;
 }
