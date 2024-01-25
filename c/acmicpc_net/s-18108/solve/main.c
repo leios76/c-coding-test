4f0c494268f324a33e13890d8683bd243749fdca
Minsoo Kim <ms.kim@hdel.co.kr>
2024-01-25 14:24:01 +0900

    백준
    - 18108	1998년생인 내가 태국에서는 2541년생?!

---
+++ c/acmicpc_net/s-18108/solve/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+struct context_t {
+    int n;
+};
+struct context_t context;
+
+int solve(struct context_t * ctx)
+{
+    return ctx->n - 543;
+}
+
+void init_data(struct context_t * ctx)
+{
+    memset(ctx, 0, sizeof(struct context_t));
+
+    scanf("%d", &ctx->n);
+}
+
+int main()
+{
+    init_data(&context);
+    printf("%d\n", solve(&context));
+
+    return 0;
+}
