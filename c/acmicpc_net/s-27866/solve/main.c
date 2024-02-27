2bc531ee9b3e80bcc9a79a7d7c74cd3c1457ed3f
Minsoo Kim <ms.kim@hdel.co.kr>
2024-02-27 14:00:17 +0900

    백준
    - 27866 문자와 문자열

---
+++ c/acmicpc_net/s-27866/solve/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef DEBUG_OUTPUT
+    #define debug(...)  printf(__VA_ARGS__)
+#else
+    #define debug(...)
+#endif
+
+struct context_t {
+    char s[1001];
+    int i;
+};
+struct context_t context;
+
+void solve(struct context_t * ctx)
+{
+    printf("%c\n", ctx->s[ctx->i - 1]);
+}
+
+void init_data(struct context_t * ctx)
+{
+    memset(ctx, 0, sizeof(struct context_t));
+    scanf("%s", ctx->s);
+    scanf("%d", &ctx->i);
+}
+
+int main(int argc, char ** argv)
+{
+    int t = 1;
+
+    //scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        init_data(&context);
+        solve(&context);
+    }
+    return 0;
+}
