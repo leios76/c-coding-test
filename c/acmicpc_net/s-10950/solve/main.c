5f6965605f0203faab757449ea99b96f25e17acb
Minsoo Kim <ms.kim@hdel.co.kr>
2024-02-27 13:50:55 +0900

    백준
    - 10950 A+B - 3

---
+++ c/acmicpc_net/s-10950/solve/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef DEBUG_OUTPUT
+    #define debug(...)  printf(__VA_ARGS__)
+#else
+    #define debug(...)
+#endif
+
+struct context_t {
+    int a;
+    int b;
+};
+struct context_t context;
+
+void solve(struct context_t * ctx)
+{
+    printf("%d\n", ctx->a + ctx->b);
+}
+
+void init_data(struct context_t * ctx)
+{
+    memset(ctx, 0, sizeof(struct context_t));
+    scanf("%d %d", &ctx->a, &ctx->b);
+}
+
+int main(int argc, char ** argv)
+{
+    int t = 1;
+
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        init_data(&context);
+        solve(&context);
+    }
+    return 0;
+}
