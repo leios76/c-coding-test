5c7f5db7b7cf9b9b1bf1bee2ed84932dd28038d3
Minsoo Kim <ms.kim@hdel.co.kr>
2024-03-10 20:13:06 +0900

    백준
    - 1874 스택 수열

---
+++ c/acmicpc_net/2403/s-01874/random/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+int max_n = 100000;
+int min_n = 1;
+
+uint32_t bitmap[100031/32];
+
+int main()
+{
+    int n;
+
+    memset(bitmap, 0, sizeof(bitmap));
+    srand(time(NULL));
+    n = rand() % (max_n - min_n) + min_n;
+
+    printf("%d\n", n);
+
+    for (int i = 0; i < n;) {
+        int v = rand() % n + 1;
+        if ((bitmap[v/32] & (1 << v)) != 0) {
+            continue;
+        }
+        printf("%d\n", v);
+        i++;
+        bitmap[v/32] |= (1 << v);
+    }
+    return 0;
+}
---
+++ c/acmicpc_net/2403/s-01874/verify/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifdef DEBUG_OUTPUT
+    #define debug(...)  printf(__VA_ARGS__)
+#else
+    #define debug(...)
+#endif
+
+struct context_t {
+    int n;
+    int current_number;
+    int numbers[100000];
+    int stack[100000];
+    int stack_pos;
+    int stack_op[1000000];
+    int stack_op_count;
+};
+struct context_t context;
+
+void solve(struct context_t * ctx)
+{
+    int result = 0;
+    ctx->stack_pos = -1;
+    for (int n = 0; n < ctx->n; n++) {
+        if (ctx->stack_pos == -1) {
+            ctx->stack[++ctx->stack_pos] = ++ctx->current_number;
+            ctx->stack_op[ctx->stack_op_count++] = 1;
+        }
+        while (ctx->stack_pos < 100000 && ctx->stack[ctx->stack_pos] != ctx->numbers[n]) {
+            ctx->stack[++ctx->stack_pos] = ++ctx->current_number;
+            ctx->stack_op[ctx->stack_op_count++] = 1;
+        }
+        if (ctx->stack_pos > -1 && ctx->stack_pos < 100000 && ctx->stack[ctx->stack_pos] == ctx->numbers[n]) {
+            ctx->stack_op[ctx->stack_op_count++] = 0;
+            ctx->stack_pos--;
+        }
+    }
+    if (ctx->stack_pos == -1) {
+        for (int c = 0; c < ctx->stack_op_count; c++) {
+            printf("%c\n", ctx->stack_op[c] ? '+':'-');
+        }
+    } else {
+        printf("NO\n");
+    }
+}
+
+void init_data(struct context_t * ctx)
+{
+    memset(ctx, 0, sizeof(struct context_t));
+    scanf("%d", &ctx->n);
+    for (int n = 0; n < ctx->n; n++) {
+        scanf("%d", &ctx->numbers[n]);
+    }
+}
+
+int main(int argc, char ** argv)
+{
+    int t = 1;
+
+    //scanf("%d", &t);
+    for (int i = 0; i < t; i++) {
+        init_data(&context);
+        solve(&context);
+    }
+    return 0;
+}
