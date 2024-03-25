7d08eb9256a6f14cc2941291cd7f9d6df907b35b
Minsoo Kim <ms.kim@hdel.co.kr>
2024-03-25 20:04:17 +0900

    cpp 파일도 컴파일하도록 sample 수정

--- c/goorm_io/sample/random/target.mk
+++ c/goorm_io/sample/random/target.mk
@@ -6,6 +6,7 @@ WORKDIR := $(WORKDIR)/$(notdir $(shell dirname $(realpath $(shell dirname $(real
 WORKDIR := $(WORKDIR)/$(notdir $(shell dirname $(realpath $(shell dirname $(lastword $(MAKEFILE_LIST))))))
 WORKDIR := $(WORKDIR)/$(notdir $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST)))))
 SOURCES += $(patsubst $(WORKDIR)/%.c, %.c, $(wildcard $(WORKDIR)/*.c))
+SOURCES += $(patsubst $(WORKDIR)/%.cpp, %.cpp, $(wildcard $(WORKDIR)/*.cpp))
 
 TGT_CFLAGS :=
 TGT_LDFLAGS := -L${BASE_DIR}/output/lib
