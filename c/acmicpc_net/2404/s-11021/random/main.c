b67cd0612e37fdd17b72448fe5b935f786a89bdb
Minsoo Kim <ms.kim@hdel.co.kr>
2024-04-22 08:25:58 +0900

    백준 - 11021 A+B - 7

---
+++ c/acmicpc_net/2404/s-11021/random/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+
+int max_n = 50;
+int max_k = 5;
+
+int get_rand(int min, int max)
+{
+    return (rand() % (max - min + 1)) + min;
+}
+
+int main()
+{
+    int n;
+    int k;
+    int ix;
+    int iy;
+
+    srand(time(NULL));
+    n = rand() % (max_n - 1) + 2;
+    k = rand() % max_k + 1;
+
+    printf("%d %d\n", n, k);
+
+    for (int i = 1; i <= k; i++) {
+        ix = rand() % n + 1;
+        iy = rand() % n + 1;
+        printf("%d %d\n", ix, iy);
+    }
+    return 0;
+}
